c540f89468e0b20ae6edbff5c65eba449e438286
juliohtm08 <jhtomoyuki345@gmail.com>
2024-12-21 02:50:36 -0300

    API REST: Rota POST

--- NodeJS_com_Web/API_REST/src/server.js
+++ NodeJS_com_Web/API_REST/src/server.js
@@ -3,12 +3,16 @@ const gamesController = require("./controllers/games-controller");
 
 const app = express();
 
+app.use(express.json()); // recebe o conteúdo em formato json e formata para um objeto javascript apropriado
+
 app.post("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
 app.get("/games", gamesController.index);
+app.post("/games", gamesController.save);
 app.get("/games/:id", gamesController.show);
+app.post("/games/:id/genres", gamesController.addGenre);
 
 const PORT = 3000;
 
